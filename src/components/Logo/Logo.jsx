ac8935ec117907cf952680d0d08e6d46f8d17735
Vita <nikaangel295@gmail.com>
2024-09-24 15:08:18 +0100

    add

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,9 @@
+
+import { Link } from "react-router-dom";
+import css from "./Logo.module.css";
+
+const Logo = () => {
+  return <Link to={`/`} className={css.logo}>AquaTrack</Link>;
+};
+
+export default Logo;
