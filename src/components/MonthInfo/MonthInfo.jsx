4c5cf1d22c0061ab19ed10accbc5397eb1ad7838
Vita <nikaangel295@gmail.com>
2024-09-24 15:25:14 +0100

    add

---
+++ src/components/MonthInfo/MonthInfo.jsx
@@ -0,0 +1,18 @@
+
+import { Outlet } from "react-router-dom";
+import CalendarPagination from "../CalendarPagination/CalendarPagination.jsx";
+import { Suspense } from "react";
+import Loader from "../Loader/Loader.jsx";
+
+const MonthInfo = () => {
+  return (
+    <div>
+      <CalendarPagination />
+      <Suspense fallback={<Loader />}>
+        <Outlet />
+      </Suspense>
+    </div>
+  );
+};
+
+export default MonthInfo;
