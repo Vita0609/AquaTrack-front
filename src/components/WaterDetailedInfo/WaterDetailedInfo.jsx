7a7bc55e3ee9fbbc8e67e0db02c83ddf1815e95b
Vita <nikaangel295@gmail.com>
2024-09-24 15:36:01 +0100

    add

---
+++ src/components/WaterDetailedInfo/WaterDetailedInfo.jsx
@@ -0,0 +1,18 @@
+
+import DailyInfo from "../DailyInfo/DailyInfo.jsx";
+import MonthInfo from "../MonthInfo/MonthInfo.jsx";
+import UserPanel from "../UserPanel/UserPanel.jsx";
+import styles from "./WaterDetailedInfo.module.css";
+
+const WaterDetailedInfo = () => {
+
+  return (
+    <section className={styles.waterDetailedInfo}>
+      <UserPanel />
+      <DailyInfo />
+      <MonthInfo />
+    </section>
+  );
+};
+
+export default WaterDetailedInfo;
