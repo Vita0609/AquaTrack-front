354baf45d24bc45b2e79798be062863664dd0dc0
Vita <nikaangel295@gmail.com>
2024-09-24 15:01:40 +0100

    add

---
+++ src/components/DailyInfo/DailyInfo.jsx
@@ -0,0 +1,18 @@
+import WaterList from "../WaterList/WaterList";
+import css from "./DailyInfo.module.css";
+import AddWaterBtn from "../AddWaterSecond/AddWaterSecond";
+import ChooseDate from "../ChooseDate/ChooseDate";
+
+const DailyInfo = () => {
+  return (
+    <div className={css.info}>
+      <div className={`${css.header} five-step`}>
+        <ChooseDate />
+        <AddWaterBtn />
+      </div>
+      <WaterList />
+    </div>
+  );
+};
+
+export default DailyInfo;
---
+++ src/components/DeleteWaterModal/DeleteWaterModal.jsx
@@ -0,0 +1,70 @@
+
+import svg from "../../assets/icons.svg";
+import css from "./DeleteWaterModal.module.css";
+import BtnDelete from "../BtnDelete/BtnDelete.jsx";
+import { ANIMATION } from "../../constants.js";
+import { useTranslation } from "react-i18next";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { deleteWaterIntakeRecord } from "../../redux/water/operations.js";
+import LoaderComponent from "../LoaderComponent/LoaderComponent.jsx";
+
+const ModalDeleteEntry = ({ id, onClose }) => {
+  const { t } = useTranslation();
+  const [isLoading, setIsLoading] = useState(false);
+  const dispatch = useDispatch();
+
+  const handleClose = () => {
+    const id = setTimeout(() => {
+      onClose();
+      clearTimeout(id);
+    }, ANIMATION.DURATION);
+  };
+
+  const handleDelete = () => {
+    setIsLoading(() => true);
+    dispatch(deleteWaterIntakeRecord(id)).then(({ error }) => {
+      if (!error) {
+        handleClose();
+      }
+      setIsLoading(false);
+    });
+  };
+
+  return (
+    <div className={css.modal}>
+      <button
+        type="button"
+        aria-label={t("closeDeleteEntryModal")}
+        onClick={handleClose}
+        className={css.closeBtn}
+      >
+        <svg className={css.svg}>
+          <use xlinkHref={svg + "#icon-x"}></use>
+        </svg>
+      </button>
+      <div className={css.modalTextBox}>
+        <h2 className={css.modalTitle}>{t("deleteEntry")}</h2>
+        <p className={css.modalText}>{t("confirmDeleteEntry")}</p>
+      </div>
+      <div className={css.modalBtnBox}>
+        {isLoading ? (
+          <LoaderComponent height={80} width={80} />
+        ) : (
+          <>
+            <BtnDelete handleDelete={handleDelete} id={id} />
+            <button
+              type="button"
+              onClick={handleClose}
+              className={css.btnCancel}
+            >
+              {t("cancel")}
+            </button>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ModalDeleteEntry;
