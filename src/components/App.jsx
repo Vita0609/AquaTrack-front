b58135fa00113e7e28e9dd594d54f06d03cca464
Vita <nikaangel295@gmail.com>
2024-09-24 15:40:13 +0100

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,35 +1,134 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import { lazy, useEffect } from "react";
+import { toast, Toaster } from "react-hot-toast";
+import { useDispatch, useSelector } from "react-redux";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { getUserInfo } from "../redux/auth/operations.js";
+import {
+  selectAuthErrorMessage,
+  selectAuthSuccessMessage,
+  selectToken,
+} from "../redux/auth/selectors.js";
+import { setLoggedIn } from "../redux/auth/slice.js";
+import PrivateRoute from "./PrivateRoute.jsx";
+import RestrictedRoute from "./RestrictedRoute.jsx";
+import SharedLayout from "./SharedLayout/SharedLayout.jsx";
+import {
+  selectDailyErrorMessage,
+  selectDailySuccessMessage,
+} from "../redux/water/selectors.js";
+
+const HomePage = lazy(() => import("../pages/HomePage/HomePage"));
+const SignInPage = lazy(() => import("../pages/SignInPage/SignInPage.jsx"));
+const SignUpPage = lazy(() => import("../pages/SignUpPage/SignUpPage.jsx"));
+const TrackerPage = lazy(() => import("../pages/TrackerPage/TrackerPage.jsx"));
+const NotFoundPage = lazy(
+  () => import("../pages/NotFoundPage/NotFoundPage.jsx")
+);
+const WaterIntakeChart = lazy(
+  () => import("./CalendarChart/CalendarChart.jsx")
+);
+const Calendar = lazy(() => import("./Calendar/Calendar.jsx"));
 
 function App() {
-  const [count, setCount] = useState(0)
+  const token = useSelector(selectToken);
+  const dispatch = useDispatch();
+  const authErrorMessage = useSelector(selectAuthErrorMessage);
+  const authSuccessMessage = useSelector(selectAuthSuccessMessage);
+  const waterErrorMessage = useSelector(selectDailyErrorMessage);
+  const waterSuccessMessage = useSelector(selectDailySuccessMessage);
+
+  useEffect(() => {
+    if (token) {
+      dispatch(getUserInfo());
+      dispatch(setLoggedIn(true));
+    }
+  }, []);
+
+  //AUTH TOAST
+  useEffect(() => {
+    if (authErrorMessage) {
+      toast.error(authErrorMessage);
+    }
+    if (authSuccessMessage) {
+      toast.success(authSuccessMessage);
+    }
+  }, [authSuccessMessage, authErrorMessage]);
+
+  //WATER TOAST
+  useEffect(() => {
+    if (waterErrorMessage) {
+      toast.error(waterErrorMessage);
+    }
+    if (waterSuccessMessage) {
+      toast.success(waterSuccessMessage);
+    }
+  }, [waterErrorMessage, waterSuccessMessage]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <SharedLayout>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+
+        <Route
+          path="/signup"
+          element={
+            <RestrictedRoute
+              redirectTo={`/tracker/${Date.now()}`}
+              component={<SignUpPage />}
+            />
+          }
+        />
+
+        <Route
+          path="/signin"
+          element={
+            <RestrictedRoute
+              redirectTo={`/tracker/${Date.now()}`}
+              component={<SignInPage />}
+            />
+          }
+        />
+
+        <Route
+          path="/tracker/:date"
+          element={
+            <PrivateRoute redirectTo="/signin" component={<TrackerPage />} />
+          }
+        >
+          <Route path="calendar" element={<Calendar />} />
+          <Route path="schedule" element={<WaterIntakeChart />} />
+          <Route index element={<Navigate to="calendar" replace />} />
+        </Route>
+
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+      <Toaster
+        toastOptions={{
+          style: {
+            padding: "16px",
+            fontWeight: "700",
+            color: "white",
+            borderRadius: "15px",
+            backgroundColor: "#323F47",
+          },
+          success: {
+            iconTheme: {
+              primary: "#9BE1A0",
+              secondary: "#FFF",
+            },
+          },
+          error: {
+            iconTheme: {
+              primary: "#EF5050",
+              secondary: "#F0EFF4",
+            },
+          },
+        }}
+        position="top-right"
+      />
+    </SharedLayout>
+  );
 }
 
-export default App
+export default App;
