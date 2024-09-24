bcffc9cc4154e248d699fe659f93f9a0bdba1928
Vita <nikaangel295@gmail.com>
2024-09-24 15:39:01 +0100

    add

---
+++ src/components/WaterMainInfo/WaterMainInfo.jsx
@@ -0,0 +1,38 @@
+
+import { useTranslation } from "react-i18next";
+import WaterProgressBar from "../WaterProgressBar/WaterProgressBar";
+import css from "./WaterMainInfo.module.css";
+import { useSelector } from "react-redux";
+import Logo from "../Logo/Logo.jsx";
+import { selectWaterRate } from "../../redux/auth/selectors.js";
+import BtnUserSet from "../BtnUserSet/BtnUserSet.jsx";
+import AddWaterBtn from "../AddWaterBtn/AddWaterBtn.jsx";
+import { useTour } from "@reactour/tour";
+
+const WaterMainInfo = () => {
+  const { t } = useTranslation();
+  const waterRate = useSelector(selectWaterRate);
+
+
+  const waterRateString =
+    waterRate < 1 ? `${waterRate * 1000} ${t("ml")}` : `${waterRate} ${t("l")}`;
+    
+  return (
+    <div className={css.waterContainer}>
+      <Logo className={css.waterTitle} />
+
+      {waterRate === 0 ? (
+        ""
+      ) : (
+        <div className={`${css.normaContainer} first-step`}>
+          <h2 className={css.normaL}>{waterRateString}</h2>
+          <p className={css.normaText}>{t("dailyNorm")}</p>
+        </div>
+      )}
+      {waterRate === 0 ? "" : <WaterProgressBar />}
+      {waterRate === 0 ? <BtnUserSet /> : <AddWaterBtn />}
+    </div>
+  );
+};
+
+export default WaterMainInfo;
---
+++ src/components/WaterProgressBar/WaterProgressBar.jsx
@@ -0,0 +1,54 @@
+
+import { useTranslation } from "react-i18next";
+import css from "./WaterProgressBar.module.css";
+import { selectWaterRate } from "../../redux/auth/selectors";
+import { useSelector } from "react-redux";
+
+const WaterProgressBar = () => {
+  const { t } = useTranslation();
+  const currentWater = useSelector((state) => state.water.todayAmount.value);
+  const goal = useSelector(selectWaterRate);
+  const calculatedPercentage = Math.round((currentWater / (goal * 1000)) * 100);
+
+  const percentage = calculatedPercentage >= 100 ? 100 : calculatedPercentage;
+
+  return (
+    <div className={`${css.progressBarContainer} second-step`}>
+      <h2 className={css.progressBarTitle}>{t("today")}</h2>
+      <div className={css.progressBar}>
+        <div
+          className={css.progressBarFill}
+          style={{
+            width: `${percentage}%`,
+            backgroundColor: currentWater < goal * 1000 ? "#9be1a0" : "#FABE4A",
+          }}
+        >
+          <p
+            className={css.percentNumber}
+            style={{
+              color: currentWater < goal * 1000 ? "#9be1a0" : "#FABE4A",
+            }}
+          >
+            {currentWater < goal * 1000 ? `${percentage.toFixed(0)}%` : "done!"}
+          </p>
+          <div
+            className={css.ball}
+            style={{
+              border:
+                currentWater < goal * 1000
+                  ? "solid 1px #9be1a0"
+                  : "solid 1px #FABE4A",
+            }}
+          ></div>
+        </div>
+      </div>
+      <div className={css.percentContainer}>
+        <p className={css.percent}>0%</p>
+        <p className={css.percent}>50%</p>
+        <p className={css.percent}>100%</p>
+      </div>
+    </div>
+  );
+};
+
+export default WaterProgressBar;
